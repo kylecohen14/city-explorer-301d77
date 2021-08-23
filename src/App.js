0b111133a15d19c321acc38922f5f765fa414f40
kylec <KYLECOHEN14@GMAIL.COM>
2021-08-23 18:19:01 -0400

    Up to card 2 step 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Card from 'react-bootstrap/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: '',
+      cityLocation: {}
+    }
+  }
+
+  getLocation = async () => {
+    const LocationAPI = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_EXPLORER_KEY}&q=${this.state.searchQuery}&format=json`;
+    const res = await axios.get(LocationAPI);
+    this.setState({ cityLocation: res.data[0] });
+  }
+  render() {
+    return (
+      <div>
+        <Card bg='info' border="primary" style={{ width: '18rem'}}>
+          <Card.Body>
+            <input onChange={(e) => this.setState({ searchQuery: e.target.value })} value={this.state.searchQuery} placeholder="search a city"></input>
+            <button onClick={this.getLocation}>Explore</button>
+            {this.state.cityLocation.place_id &&
+            <>
+              <Card.Title>City Name: {this.state.cityLocation.display_name}</Card.Title>
+              <Card.Text>Latitude: {this.state.cityLocation.lat}</Card.Text>
+              <Card.Text>Longitude: {this.state.cityLocation.lon}</Card.Text>
+            </>
+              }
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default App;
